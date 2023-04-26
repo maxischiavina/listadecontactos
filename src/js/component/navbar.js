7e538d3f8e877d709527a1fefb67aa68b6925ec6
maxischiavina <123894494+maxischiavina@users.noreply.github.com>
2023-04-26 19:02:53 +0000

    Primer commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import {usecontext} from "react";
+// import {Context} from "../store/appContext.js"
 
 export const Navbar = () => {
+	// function prueba(){
+	// 	console.log(context)
+	// }
+	// const {prueba} = useContext(context)
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<form>
+			<h1>Add a new contact</h1>
+  <div className="mb-3">
+    <label for="fullName" className="form-label">Full Name</label>
+    <input type="email" className="form-control" id="fullName" aria-describedby="emailHelp" />
+  </div>
+
+  <div className="mb-3">
+    <label for="exampleInputEmail1" className="form-label">Email</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+    
+  </div>
+  <div className="mb-3">
+    <label for="phone" className="form-label">Phone</label>
+    <input type="number" className="form-control" id="phone" aria-describedby="emailHelp" />
+   
+  </div>
+  <div className="mb-3">
+    <label for="Adress" className="form-label">Adress</label>
+    <input type="" className="form-control" id="exampleInputPassword1" />
+  </div>
+  
+  <button type="submit" className="btn btn-primary">Save</button>
+</form>
+
 	);
 };
